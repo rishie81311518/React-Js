7f0e8798cc3aff1cd1adf192f70e0fffdb2c8584
Rishi <rishiekandala@gmail.com>
2024-01-12 21:30:27 +0530

    class components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import logo from './logo.svg';
 import Greet from './components/Greet'
+import Welcome from './components/Welcome'
 
 function App() {
   return (
     <div className="App">
       <Greet/>
+      <Welcome/>
     </div>
   );
 }
