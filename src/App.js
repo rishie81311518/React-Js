683dc4106dfa6066b538562bd6c4882cbdb3f9d8
Rishi <rishiekandala@gmail.com>
2024-01-13 10:35:39 +0530

    JSX

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import logo from './logo.svg';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
+import Hello from './components/Hello'
 
 function App() {
   return (
     <div className="App">
-      <Greet/>
-      <Welcome/>
+      {/* <Greet/>
+      <Welcome/> */}
+    <Hello/>
+    
     </div>
   );
 }
