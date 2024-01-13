9ea5dab382cc3699dc17a2cdd5f50b3f00ec81d9
Rishi <rishiekandala@gmail.com>
2024-01-13 10:53:28 +0530

    props with functional components

--- src/App.js
+++ src/App.js
@@ -7,10 +7,16 @@ import Hello from './components/Hello'
 function App() {
   return (
     <div className="App">
-      {/* <Greet/>
-      <Welcome/> */}
-    <Hello/>
-    
+      <Greet name="Aman" heroName ="superman" >
+        <p>This is Aman</p>
+      </Greet>
+      <Greet name="Pratham" heroName = "batman" >
+      <button>Action</button>
+      </Greet>
+      <Greet name="Shresta" heroName ="hawkeye" />
+      {/* <Welcome/>
+    <Hello/> */}
+
     </div>
   );
 }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-// function Greet() {
-//     return <h1>Hello Rishie</h1>
-// }
+const Greet = props => {
+    console.log(props)
+    return (
+        <div>
+        <h1>Hello {props.name} aka {props.heroName}</h1>
+        {props.children}
+        </div>
+    )
+    
+}
 
-const Greet = () => <h1>Hello Rishie</h1>
+
 
 export default Greet
