ff91f0887afe2c2bd261184ce66222253f98fdd7
Rishi <rishiekandala@gmail.com>
2024-01-16 10:06:28 +0530

    component updating lifecycle

--- src/components/LifecycleA.js
+++ src/components/LifecycleA.js
@@ -21,12 +21,33 @@ componentDidMount() {
     console.log('LifecycleA componentDidMount')
 }
 
+shouldComponentUpdate() {
+    console.log('LifecycleA shouldComponentUpdate')
+    return true
+}
+
+getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('LifecycleA getSnapshotBeforeUpdate')
+    return null
+}
+
+componentDidUpdate() {
+    console.log('LifecycleA componentDidUpdate')
+}
+
+changeState = () => {
+    this.setState({
+        name:'Codevoultion'
+    })
+}
+
 
   render() {
     console.log('LifecycleA render')
     return (
         <div>
             <div>LifecycleA</div>
+            <button onClick={this.changeState}> Change state</button>
        <LifecycleB/>
        </div>
      
--- src/components/LifecycleB.js
+++ src/components/LifecycleB.js
@@ -20,6 +20,20 @@ componentDidMount() {
     console.log('LifecycleB componentDidMount')
 }
 
+shouldComponentUpdate() {
+    console.log('LifecycleB shouldComponentUpdate')
+    return true
+}
+
+getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('LifecycleB getSnapshotBeforeUpdate')
+    return null
+}
+
+componentDidUpdate() {
+    console.log('LifecycleB componentDidUpdate')
+
+}
 
   render() {
     console.log('LifecycleB render')
