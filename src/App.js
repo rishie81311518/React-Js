bae5f7ea68def1be1b209723a2464d3a3c3b62c8
Rishi <rishiekandala@gmail.com>
2024-01-27 08:52:10 +0530

    get request

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
-import { UserProvider } from "./components/userContext";
-import ComponentC from "./components/ComponentC";
+import PostList from "./components/PostList";
 
 class App extends Component {
   render() {
     return <div className="App">
-      <UserProvider value="Vishwas">
-        <ComponentC/>
-      </UserProvider>
+      <PostList/>
     </div>;
   }
 }
